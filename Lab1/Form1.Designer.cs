a68e1f33c5a03b5bf022621fd3320e4609d257c9
erp-nikoda <enikoda@gmail.com>
2022-08-18 20:32:10 +0300

    Добавьте файлы проекта.

---
+++ Lab1/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace Lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnSelectFile = new System.Windows.Forms.Button();
+            this.btnCalcStat = new System.Windows.Forms.Button();
+            this.btnSaveStat = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSelectFile
+            // 
+            this.btnSelectFile.BackgroundImage = global::Lab1.Properties.Resources.Button001;
+            this.btnSelectFile.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnSelectFile.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSelectFile.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnSelectFile.Location = new System.Drawing.Point(46, 83);
+            this.btnSelectFile.Name = "btnSelectFile";
+            this.btnSelectFile.Size = new System.Drawing.Size(177, 34);
+            this.btnSelectFile.TabIndex = 0;
+            this.btnSelectFile.Text = "Обрати файл";
+            this.btnSelectFile.UseVisualStyleBackColor = true;
+            this.btnSelectFile.Click += new System.EventHandler(this.btnSelectFile_Click);
+            // 
+            // btnCalcStat
+            // 
+            this.btnCalcStat.AutoEllipsis = true;
+            this.btnCalcStat.BackgroundImage = global::Lab1.Properties.Resources.Button001;
+            this.btnCalcStat.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnCalcStat.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCalcStat.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnCalcStat.Location = new System.Drawing.Point(46, 133);
+            this.btnCalcStat.Name = "btnCalcStat";
+            this.btnCalcStat.Size = new System.Drawing.Size(177, 47);
+            this.btnCalcStat.TabIndex = 1;
+            this.btnCalcStat.Text = "Отримати статистику файла";
+            this.btnCalcStat.UseVisualStyleBackColor = true;
+            this.btnCalcStat.Click += new System.EventHandler(this.btnCalcStat_Click);
+            // 
+            // btnSaveStat
+            // 
+            this.btnSaveStat.BackgroundImage = global::Lab1.Properties.Resources.Button001;
+            this.btnSaveStat.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnSaveStat.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSaveStat.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnSaveStat.Location = new System.Drawing.Point(168, 260);
+            this.btnSaveStat.Name = "btnSaveStat";
+            this.btnSaveStat.Size = new System.Drawing.Size(177, 30);
+            this.btnSaveStat.TabIndex = 2;
+            this.btnSaveStat.Text = "Зберегти статистику";
+            this.btnSaveStat.UseVisualStyleBackColor = true;
+            this.btnSaveStat.Click += new System.EventHandler(this.btnSaveStat_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(249, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(208, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Результат выбору файла:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(249, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(264, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Результат отримання статистики:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(245, 192);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(268, 18);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Статистику збережено до файлу:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(236, 89);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(355, 26);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(236, 148);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(355, 26);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox3.Location = new System.Drawing.Point(137, 213);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(454, 26);
+            this.textBox3.TabIndex = 8;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "Текст.txt";
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::Lab1.Properties.Resources.Button001;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(447, 260);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 30);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Вихід";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = global::Lab1.Properties.Resources.S_logo;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(17, 196);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.label4.Location = new System.Drawing.Point(0, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(601, 24);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "ПРОГРАМА ДЛЯ ПІДРАХУНКУ БУКВ У ТЕКСТОВОМУ ФАЙЛІ";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.BackgroundImage = global::Lab1.Properties.Resources._1;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(12, 79);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(26, 40);
+            this.pictureBox2.TabIndex = 12;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.BackgroundImage = global::Lab1.Properties.Resources._2;
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox3.Location = new System.Drawing.Point(11, 133);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(30, 45);
+            this.pictureBox3.TabIndex = 13;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox4.BackgroundImage = global::Lab1.Properties.Resources._3;
+            this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox4.Location = new System.Drawing.Point(132, 251);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(30, 45);
+            this.pictureBox4.TabIndex = 14;
+            this.pictureBox4.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Lab1.Properties.Resources.bgrnd;
+            this.ClientSize = new System.Drawing.Size(607, 314);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSaveStat);
+            this.Controls.Add(this.btnCalcStat);
+            this.Controls.Add(this.btnSelectFile);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Статистика текстового файлу";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelectFile;
+        private System.Windows.Forms.Button btnCalcStat;
+        private System.Windows.Forms.Button btnSaveStat;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+    }
+}
+
---
+++ Lab1/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Lab1
+{
+    public partial class Form1 : Form
+    {
+        string selectfilename;
+        string resultfilename;
+        int commoncount;
+        int charcount;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // 1) кнопка выбора файла Текст.тхт с формы
+        private void btnSelectFile_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            // получаем выбранный файл
+            selectfilename = openFileDialog1.FileName;
+            textBox1.Text = selectfilename;
+            // обнуляем данные для нового подсчета
+            commoncount = 0; charcount = 0;
+            textBox2.Text = "";
+            textBox3.Text = "";
+        }
+
+        // 2) для подсчета количества всех символов в файле Текст.тхт
+        private void btnCalcStat_Click(object sender, EventArgs e)
+        {
+            if (selectfilename != null)
+            {
+                // читаем файл в строку
+                string fileText = File.ReadAllText(selectfilename, Encoding.Default);
+
+                // в цикле подсчитываем каждый символ строки
+                foreach (char symbol in fileText)
+                {
+                    commoncount++; // получаем общее число символов
+
+
+                    // отдельно считаем количество букв
+                    if (Char.IsLetter(symbol)) charcount++;// получаем общее число букв
+                }
+                // 3) окно для вывода результатов подсчета 
+                string resultText = "Загальна кількість символіву файлі: " + commoncount.ToString() + "\n";
+                resultText += "Кількість букв у файлі: " + charcount.ToString();
+                textBox2.Text = "Підрахунок завершено успішно.";
+                MessageBox.Show(resultText, "Результат підрахунку символів у файлі");
+            }           
+        }
+
+        // 4) кнопка для записи результата в файл Результат.тхт 
+        private void btnSaveStat_Click(object sender, EventArgs e)
+        {
+            if (selectfilename != null)
+            {
+                // создаем файл Результат.txt в той же папке, что и файл Текст.тхт
+                resultfilename = selectfilename + "Результат.txt";
+
+                // Формируем строки для записи в файл  Результат.txt
+                string resultText = "Файл " + selectfilename + "\r\n";
+                resultText += "\r\n" + "Загальна кількість символіву файлі: " + commoncount.ToString() + "\r\n";
+                resultText += "Кількість букв у файлі: " + charcount.ToString();
+                // сохраняем статистику в файл
+                File.WriteAllText(resultfilename, resultText);
+                textBox3.Text = resultfilename;
+            }            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
